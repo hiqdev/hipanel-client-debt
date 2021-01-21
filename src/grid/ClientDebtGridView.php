d2efe1db5e8488e66bd0dc4fb3e52b52bdb9d866
Andrey Klochok <andreyklochok@gmail.com>
2021-01-21 12:14:42 +0200

    Fix link to client on the index page

--- src/grid/ClientDebtGridView.php
+++ src/grid/ClientDebtGridView.php
@@ -12,14 +12,21 @@ namespace hipanel\client\debt\grid;
 
 use hipanel\modules\client\grid\ClientGridView;
 use hipanel\helpers\Url;
+use hipanel\modules\client\models\Client;
 use yii\helpers\Html;
 use Yii;
 
 class ClientDebtGridView extends ClientGridView
 {
-    public function columns()
+    public function columns(): array
     {
         return array_merge(parent::columns(), [
+            'login_without_note' => [
+                'attribute' => 'login',
+                'filterAttribute' => 'login_ilike',
+                'format' => 'raw',
+                'value' => fn(Client $client): string => Html::a($client->login, ['@client/view', 'id' => $client->id], ['class' => 'text-bold']),
+            ],
             'payment_ticket' => [
                 'format' => 'html',
                 'label' => Yii::t('hipanel', 'Ticket'),
@@ -41,7 +48,7 @@ class ClientDebtGridView extends ClientGridView
                         $class = 'text-blue';
                     }
 
-                    return  Html::a($model->payment_ticket_id, Url::to(['@ticket/view', 'id' => $model->payment_ticket_id]), compact('class'));
+                    return Html::a($model->payment_ticket_id, Url::to(['@ticket/view', 'id' => $model->payment_ticket_id]), compact('class'));
                 },
             ],
             'payment_status' => [
@@ -59,7 +66,7 @@ class ClientDebtGridView extends ClientGridView
                     }
 
                     return Yii::t('hipanel.debt', 'Debt');
-                }
+                },
             ],
             'ticket_status' => [
                 'label' => Yii::t('hipanel.debt', 'Ticket state'),
@@ -71,7 +78,7 @@ class ClientDebtGridView extends ClientGridView
                         return '';
                     }
 
-                    $state = $model->payment_ticket->state === 'opened' ? Yii::t('hipanel.debt', 'Opened') :  Yii::t('hipanel.debt', 'Closed');
+                    $state = $model->payment_ticket->state === 'opened' ? Yii::t('hipanel.debt', 'Opened') : Yii::t('hipanel.debt', 'Closed');
                     $status = $model->payment_ticket->status === 'wait_admin' ? Yii::t('hipanel.debt', 'Wait for staff') : Yii::t('hipanel.debt', 'Wait for client');
 
                     return "{$state} / {$status}";
@@ -86,11 +93,11 @@ class ClientDebtGridView extends ClientGridView
             'debt_depth' => [
                 'filter' => false,
                 'format' => 'html',
-                'value' => function($model) {
+                'value' => function ($model) {
                     if ($model->balance >= 0) {
                         return '';
                     }
-                    if ($model->debt_depth === null || (int) $model->debt_depth > 1000) {
+                    if ($model->debt_depth === null || (int)$model->debt_depth > 1000) {
                         return Html::tag('span', Yii::t('hipanel:client', '&#8734;'), ['class' => 'text-red']);
                     }
 
@@ -101,10 +108,11 @@ class ClientDebtGridView extends ClientGridView
                 'format' => 'html',
                 'filter' => false,
                 'label' => Yii::t('hipanel.debt', 'Sold services'),
-                'value' => function($model) {
+                'value' => function ($model) {
                     foreach (json_decode($model->sold_services, true) as $sold_service => $value) {
                         $sold_services[] = Html::tag('span', strtoupper(substr($sold_service, 0, 1)), ['class' => $value ? 'text-green text-bold' : 'text-red']);
                     }
+
                     return implode(' / ', $sold_services);
                 },
             ],
